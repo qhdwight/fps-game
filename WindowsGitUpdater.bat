cbc668ef79454dabb7ba0355ae482ebc7a880225
Quintin Dwight <qhdwight@gmail.com>
2017-02-22 21:49:36 -0800

    Updated world edit

--- WindowsGitUpdater.bat
+++ WindowsGitUpdater.bat
@@ -1,8 +1,15 @@
 echo off
 echo.
 echo This program updates fps-game
+echo You must have git added to cmd
 echo.
 
 git reset --hard
 git clean -f
 git pull
+
+echo.
+echo Successfully updated!
+echo.
+
+pause
